c2863adb7843d7854c8bde934fbe589470d954f1
Harsh-Knarsh <theeSkin@proton.me>
2025-02-17 00:10:31 -0600

    Update main.lua

--- grid_move_and_push/main.lua
+++ grid_move_and_push/main.lua
@@ -1,15 +1,27 @@
-grid = {cells = {}, 
+grid = {cells = {}, obstacles = {{x = 2, y = 8}}, player = {x = 1, y = 1},
   dims = {
-    cellW = 25,
-    cellH = 5,
-    gridW = 5,
-    gridH = 5,
+    cellW = 10,
+    cellH = 10,
+    gridW = 25,
+    gridH = 25,
     pad = 1
     }
   }
 
 function love.draw()
   for i in pairs(grid.cells) do
+    love.graphics.setColor(1,1,1)
+    
+    if grid.player.x == grid.cells[i].x / (grid.dims.cellW + grid.dims.pad) and grid.player.y == grid.cells[i].y / (grid.dims.cellH + grid.dims.pad) then
+      love.graphics.setColor(0,0,1)
+    end
+    
+    for u in pairs(grid.obstacles) do
+       if grid.obstacles[u].x == grid.cells[i].x / (grid.dims.cellW + grid.dims.pad) and grid.obstacles[u].y == grid.cells[i].y / (grid.dims.cellH + grid.dims.pad) then
+         love.graphics.setColor(1,0,0)
+      end
+    end
+    
     love.graphics.rectangle("fill", grid.cells[i].x, grid.cells[i].y, grid.dims.cellW, grid.dims.cellH)
   end
 end
@@ -28,5 +40,8 @@ function grid_generate()
   end
 end
 
-
 grid_generate()
+
+for i in pairs(grid.cells) do
+  --print(grid.cells[i].x / (grid.dims.cellW + grid.dims.pad))
+end
